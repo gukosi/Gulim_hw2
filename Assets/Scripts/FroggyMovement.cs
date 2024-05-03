4e6d255ef44408972c267f28e8eb875bcf0b9765
gukosi <nurgazinag23@gmail.com>
2024-05-03 23:32:37 +0500

    upd 1.0
    conversion to android game

--- Assets/Scripts/FroggyMovement.cs
+++ Assets/Scripts/FroggyMovement.cs
@@ -15,16 +15,16 @@ public class FroggyMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5.5f;
     [SerializeField] private float jumpForce = 11f;
 
-    private enum MovementState { idle, running, jumping, falling} //finite set of animation states, convenient since animations are mutually exclusive
+    private enum MovementState { idle, running, jumping, falling } //finite set of animation states, convenient since animations are mutually exclusive
 
     [SerializeField] private AudioSource jumpSound;
     // Start is called before the first frame update
     private void Start()
     {
-       rigb =  GetComponent<Rigidbody2D>();
-       boxcoll =  GetComponent<BoxCollider2D>();
-       spriter = GetComponent<SpriteRenderer>();
-       anim = GetComponent<Animator>();
+        rigb = GetComponent<Rigidbody2D>();
+        boxcoll = GetComponent<BoxCollider2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,7 +32,7 @@ public class FroggyMovement : MonoBehaviour
     {
         dirX = Input.GetAxisRaw("Horizontal"); //from Input Manager of Unity; float for adaptability for joysticks
         rigb.velocity = new Vector2(dirX * moveSpeed, rigb.velocity.y); /*instead of 0, keep position of y the same
-                                                                 '* dirX' saves time since dirX gives pos/neg value, efficient*/ 
+                                                                 '* dirX' saves time since dirX gives pos/neg value, efficient*/
 
         if (Input.GetButtonDown("Jump") && IsGrounded()) //from Input Manager of Unity
         {
@@ -58,7 +58,7 @@ public class FroggyMovement : MonoBehaviour
         }
         else
         {
-           state = MovementState.idle;
+            state = MovementState.idle;
         }
 
         if (rigb.velocity.y > .1f) //check for disposition on Y-axis, that is jump/fall
