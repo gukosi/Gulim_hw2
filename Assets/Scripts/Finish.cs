27f02a7ab1808e547036fbdd3f29c32b7c7bc611
gukosi <143037364+gukosi@users.noreply.github.com>
2024-05-24 23:50:37 +0500

    final proj

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -17,6 +17,7 @@ public class Finish : MonoBehaviour
     {
         if (collision.gameObject.name == "Froggy" && !levelCompleted)
         {
+            UnlockNewLevel();
             finishSound.Play();
             levelCompleted = true;
             Invoke("CompleteLvl", 2f);
@@ -26,5 +27,15 @@ public class Finish : MonoBehaviour
     private void CompleteLvl()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
+        }
     }
 }
